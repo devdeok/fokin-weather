dc6577baffdb9019ae7efacfc352bab25b272fab
jaedeok95 <dlwoejr700@gmail.com>
2021-08-10 20:55:00 +0900

    temperature

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import {Alert} from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "52479588688995e4086ceec8ab61a0cd";
 
 //container는 parent
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  state={
+    isLoading: true
+    
+  };
+
+  getWeather = async(latitude, longitude) =>{
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+      this.setState({isLoading:false, temp: data.main.temp})
+  };
+
+  getLocation = async() =>{
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      // await functionality
+      const {
+        coords:{latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+      // Send to API and get weather
+    } catch(error){
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };   
+
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render(){
+    const{isLoading, temp}=this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
+  }
+
 }
 
 // flex:1은 모든 공간 사용을 의미, 레이아웃을 짤 때 사용
